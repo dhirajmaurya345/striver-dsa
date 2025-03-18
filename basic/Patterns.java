3f5912a692ad255ca4a7e5b86b4cd903c5686c76
Dheeraj Maurya <dhirajmaurya345@gmail.com>
2025-03-19 03:43:07 +0530

    2d number pattern completed

--- basic/Patterns.java
+++ basic/Patterns.java
@@ -5,6 +5,8 @@ public class Patterns {
     public static void main(String[] args){
         int rows=4;
         int cols=4;
+        int counter=1;
+        String str="ABCDE";
 //*****
 //*****
 //*****
@@ -191,23 +193,131 @@ public class Patterns {
 //            }
 //            System.out.println();
 //        }
+//1
+//01
+//101
+//0101
+//        int x=1;
+//            for(int j=0;j<=rows;j++){
+//                if(j%2==0){
+//                    x=0;
+//                }else {
+//                    x=1;
+//                }
+//                for(int i=1;i<=j;i++){
+//                    System.out.print(x);
+//                    x=1-x;
+//                }
+//                System.out.println();
+//            }
 
-        for(int i=1;i<=rows;i++){
-            for(int j=2;j<=i;j++){
-                System.out.print('*');
-            }
-            for(int j=i;j<=cols;j++){
-                System.out.print(' ');
-            }
-            System.out.println();
-        }
 
-        for (int i=1;i<=rows;i++){
-            for(int j=i;j<=cols;j++){
-                System.out.print('*');
-            }
-            for(int j=1;j<=i;j++){
-                System.out.print(' ');
+//1       1
+//12     21
+//123   321
+//1234 4321
+
+//            for(int j=0;j<=rows;j++){
+//
+//                for(int i=1;i<=j;i++){
+//                    System.out.print(i);
+//                }
+//                for(int i=j;i<=cols;i++){
+//                    System.out.print(' ');
+//                }
+//                for(int i=j+1;i<=cols;i++){
+//                    System.out.print(' ');
+//                }
+//                for(int i=1,x=j;i<=j;i++,x--){
+//                    System.out.print(x);
+//                }
+//                System.out.println();
+//            }
+//1
+//2 3
+//4 5 6
+//7 8 9 10
+
+//        for(int j=0;j<rows;j++){
+//
+//                for(int i=0;i<j;i++,counter++){
+//                    System.out.print(counter);
+//                    System.out.print(' ');
+//                }
+//                for(int i=j;i<cols;i++){
+//                    System.out.print(' ');
+//                }
+//
+//                System.out.println();
+//            }
+
+//A
+//AB
+//ABC
+//ABCD
+//ABCDE
+
+//        for(int j=0;j<rows;j++){
+//            for(int i=0;i<j;i++){
+//                System.out.print(str.charAt(i));
+//            }
+//            for(int i=j;i<cols;i++){
+//                System.out.print(' ');
+//            }
+//
+//            System.out.println();
+//        }
+//ABCD
+//ABC
+//AB
+//A
+//        for(int j=0;j<rows;j++){
+//
+//            for(int i=0;i<cols-j;i++){
+//                System.out.print(str.charAt(i));
+//            }
+//            for(int i=0;i<j;i++){
+//                System.out.print(' ');
+//            }
+//
+//            System.out.println();
+//        }
+
+//A
+//BB
+//CCC
+//DDDD
+//        for(int j=0;j<rows;j++){
+//            for(int i=0;i<=j;i++){
+//                System.out.print(str.charAt(j));
+//            }
+//            for(int i=j;i<cols;i++){
+//                System.out.print(' ');
+//            }
+//            System.out.println();
+//        }
+
+//     A
+//    ABA
+//   ABCBA
+//  ABCDCBA
+
+//        for(int j=0;j<rows;j++){
+//            for(int i=j;i<=cols;i++){
+//                System.out.print(' ');
+//            }
+//            for(int i=0;i<j;i++){
+//                System.out.print(str.charAt(i));
+//            }
+//            for(int i=0,x=j;i<=j;i++,x--){
+//                System.out.print(str.charAt(x));
+//            }
+//            System.out.println();
+//        }
+
+        for(int j=0;j<rows;j++){
+            for(int i=0,x=j;i<=j;i++,x--){
+                System.out.print(str.charAt(x));
             }
             System.out.println();
         }
